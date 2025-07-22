8e0d672663ae56bc7be6476e1f67289f85a36131
prem273 <kumarjhaprem383@gmail.com>
2025-07-22 12:12:38 +0530

    Add files via upload

---
+++ p2.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+
+void rotateArr(int arr[], int n, int d) {
+    for (int i = 0; i < d; i++) {
+      
+        // Left rotate the array by one position
+        int first = arr[0];
+        for (int j = 0; j < n - 1; j++) {
+            arr[j] = arr[j + 1];
+        }
+        arr[n - 1] = first;
+    }
+}
+
+int main() {
+    int arr[] = { 1, 2, 3, 4, 5, 6 };
+    int d = 4;
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    rotateArr(arr, n, d);
+
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
